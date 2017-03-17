b7d1230bad63fb0afc27a30ec7625e6d22da0785
carlwoll <carlw@wolfram.com>
2017-03-17 01:01:25 -0700

    Refactored StackExchange.m so that it just Gets other packages

---
+++ StackExchange/StackExchange.m
@@ -0,0 +1,11 @@
+(* ::Package:: *)
+
+BeginPackage["StackExchange`"]
+
+InstallStylesheet::usage = "InstallStylesheet[] opens the Stack Exchange style sheet"
+StackExchangeView::usage 
+
+Get["StackExchange`StylesheetFunctions`"]
+Get["StackExchange`StackExchangeView`"]
+
+EndPackage[]
