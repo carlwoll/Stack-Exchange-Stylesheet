9b12b5710308a8e8977be5668df80f4225114088
carlwoll <carlw@wolfram.com>
2017-03-17 07:46:37 -0700

    Incorporate StackExchangeView

--- StackExchange/StackExchange.m
+++ StackExchange/StackExchange.m
@@ -3,7 +3,9 @@
 BeginPackage["StackExchange`"]
 
 InstallStylesheet::usage = "InstallStylesheet[] opens the Stack Exchange style sheet"
+StackExchangeView::usage 
 
 Get["StackExchange`StylesheetFunctions`"]
+Get["StackExchange`StackExchangeView`"]
 
 EndPackage[]
