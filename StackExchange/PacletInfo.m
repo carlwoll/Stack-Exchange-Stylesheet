3b8cd5cd6517c51b36776a196cabc58dbdaff793
Carl Woll <carlw@wolfram.com>
2017-03-20 12:43:07 -0700

    Update to release 0.1.1

--- StackExchange/PacletInfo.m
+++ StackExchange/PacletInfo.m
@@ -1,14 +1,19 @@
+(* Paclet Info File *)
+
+(* created 2017/03/20*)
+
 Paclet[
-	Name -> "StackExchange",
-	Version -> "0.0.2",
-	MathematicaVersion -> "10.0+",
-	Description -> "Support WL <-> StackExchange transfer",
-	Creator -> "Carl Woll",
-	Extensions -> 
-		{
-		{"Documentation"},
-		{"Kernel", Root -> ".", Context -> "StackExchange`"},
-		{"FrontEnd"}
-		}
+    Name -> "StackExchange",
+    Version -> "0.1.1",
+    MathematicaVersion -> "10.0+",
+    Description -> "Support WL <-> StackExchange transfer",
+    Creator -> "Carl Woll",
+    Extensions -> 
+        {
+            {"Documentation"}, 
+            {"Kernel", Root -> ".", Context -> "StackExchange`"}, 
+            {"FrontEnd"}
+        }
 ]
+
 
